e2cbd515dfb047a26d897b1561f8bb45bd9c8e98
maykon-jds <maykondias2001@gmail.com>
2024-09-23 21:55:01 -0300

    SAVEPOINT

--- exercicios/aula03/exercicio02/exercicio.py
+++ exercicios/aula03/exercicio02/exercicio.py
@@ -4,49 +4,102 @@ import os
 class Aluno:
 
     def __init__(self,  nome: str, email: str, curso: str) -> None:
-        self._nome  = nome 
-        self._email = email 
-        self._curso = curso 
+        """
+        Inicializa um novo aluno com nome, email e curso.
+
+        Parameters
+        ----------
+        nome : str
+            O nome do aluno.
+        email : str
+            O email do aluno.
+        curso : str
+            O curso do aluno.
+        """
+
+        self._nome  = nome
+        self._email = email
+        self._curso = curso
 
     def getAluno(self) -> dict:
 
+        """
+        Retorna um dicionario com os dados do aluno.
+
+        Returns
+        -------
+        dict
+            Um dicionario com chaves 'nome', 'email' e 'curso'.
+        """
         return dict(nome = self._nome, email = self._email, curso = self._curso)
-    
+
     def __str__(self) -> str:
-        
+
+        """
+        Retorna um dicionario com os dados do aluno.
+
+        Returns
+        -------
+        dict
+            Um dicionario com chaves 'nome', 'email' e 'curso'.
+        """
         return str(self.getAluno())
 
-class Arquivo: 
+class Arquivo:
 
     def __init__(self, arquivo: str) -> None:
-        
+
         self._arquivo = arquivo
 
     def escreverArquivo(self, dados: dict) -> None:
-    
+
+        """
+        Escreve os dados em um arquivo no formato JSON.
+
+        Parameters
+        ----------
+        dados : dict
+            Os dados a serem escritos no arquivo.
+
+        Returns
+        -------
+        None
+        """
         arquivoAberto = open(self._arquivo, "+tr")
 
         qtdLinhas = len(arquivoAberto.readlines())
 
         dadosJson = json.dumps(dados, indent=4)
 
-# TODO: Resolver problema de contagem de linhas do arquivo
-        print(qtdLinhas)
-
         ajuste = ""
 
         if(qtdLinhas != 0):
             ajuste = ","
-            
+
 
         arquivoAberto.write(f"{ajuste}{dadosJson}\n")
 
     def showArquivo(self) -> None:
 
+        """
+        Mostra o conteudo do arquivo.
+
+        Returns
+        -------
+        None
+        """
         print(self.getArquivo())
 
     def getArquivo(self) -> str:
 
+        """
+        Retorna o conteudo do arquivo.
+
+        Returns
+        -------
+        str
+            O conteudo do arquivo.
+        """
         arquivoAberto = open(self._arquivo, "r")
 
         return arquivoAberto.read()
@@ -56,12 +109,23 @@ class Main():
 
     def __init__(self) -> None:
 
-        self.arquivo = Arquivo("/workspaces/projeto_rad/exercicios/aula03/exercicio02/arquivo.txt")
+        self.arquivo = Arquivo("exercicios/aula03/exercicio02/arquivo.txt")
 
         self.loop()
 
     def loop(self) -> None:
 
+        """
+        Realiza um loop infinito para que o usuario possa realizar as ações de:
+        - Cadastrar Aluno
+        - Listar Aluno
+        - Pesquisar Aluno
+        - Encerrar Programa
+
+        Returns
+        -------
+        None
+        """
         while True:
 
             print("1 - Cadastrar Aluno\n")
@@ -79,13 +143,28 @@ class Main():
                 case 3:
                     pass
                 case 4:
+                    os.system('cls')
                     print("Programa finalizado!")
                     break
                 case _:
                     print(f"\nValor \"{opcao}\" inválido!\n")
 
-    def cadastrarAluno(self):  
-        
+            os.system('cls')
+
+    def cadastrarAluno(self):
+
+        """
+        Responsável por cadastrar um novo aluno no arquivo de texto.
+
+        Pedirá para o usuário informar o nome, email e curso do novo aluno.
+        Em seguida, criará um novo objeto do tipo Aluno e chamará o método
+        escreverArquivo para adicionar as informações do novo aluno no
+        arquivo de texto.
+
+        Returns
+        -------
+        None
+        """
         nomeAluno = input(f"Digite o nome do novo aluno: ")
         emailAluno = input(f"Digite o email do novo aluno: ")
         cursoAluno = input(f"Digite o curso do novo aluno: ")
@@ -96,6 +175,15 @@ class Main():
 
     def loopCadastarAluno(self):
 
+        """
+        Realiza um loop para que o usuario possa realizar o cadastro de
+        vários alunos seguidos. O loop somente é encerrado quando o usuario
+        digita 'n' como resposta.
+
+        Returns
+        -------
+        None
+        """
         resposta = 0
 
         while True:
@@ -104,7 +192,6 @@ class Main():
                 case 0 | "s":
                     self.cadastrarAluno()
                 case 'n':
-                    print("Programa finalizado!")
                     break
                 case _:
                     print(f"Valor {resposta} inválido!")
@@ -113,22 +200,53 @@ class Main():
 
     def showAlunos(self):
 
-       os.system('clear')
+        """
+        Mostra a lista de alunos cadastrados.
 
-       alunosJson = str(f"{self.arquivo.getArquivo()}")
+        A lista de alunos é carregada do arquivo de dados em formato JSON.
+        Os dados são mostrados na tela em uma lista com o nome, email e curso
+        de cada aluno.
 
-       alunos = json.loads(str(f"[{alunosJson}]"))
+        Após a lista ser mostrada, o programa aguarda a entrada do usuario,
+        e logo em seguida, volta para o menu principal.
 
-       print(f"\nLista de Alunos:\n")
-       
-       for index, aluno in enumerate(alunos, start=1):
-           
-           print(f"Aluno {index} | Nome: {aluno['nome']}, Email: {aluno['email']}, Curso: {aluno['curso']}")
+        Returns
+        -------
+        None
+        """
+        os.system('cls')
 
-       print(f"\n")
+        alunosJson = str(f"{self.arquivo.getArquivo()}")
 
-       input("Voltar para Menu! (Aperte qualquer tecla)")
+        alunos = json.loads(str(f"[{alunosJson}]"))
 
-        
+        print(f"\nLista de Alunos:\n")
+
+        for index, aluno in enumerate(alunos, start=1):
+
+            print(f"Aluno {index} | Nome: {aluno['nome']}, Email: {aluno['email']}, Curso: {aluno['curso']}")
+
+        print(f"\n")
+
+        input("Voltar para Menu! (Aperte qualquer tecla)")
+
+        os.system('cls')
+
+    def pesquisarAluno(alunos, campo, valor):
+
+        match campo:
+            case 'nome':
+                resultados = [aluno for aluno in alunos if aluno.nome == valor]
+            case 'email':
+                resultados = [aluno for aluno in alunos if aluno.email == valor]
+            case 'curso':
+                resultados = [aluno for aluno in alunos if aluno.curso == valor]
+            case _:
+                print(f"Campo {campo} inválido!")
+
+        if resultados and campo in ('nome', 'email', 'curso'):
+            return resultados
+        else:
+            return f"Aluno com o {campo} '{valor}' não encontrado."
+
 Main()
-
